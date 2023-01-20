1e95d5a48aa4546ffa994b432beedafeb26b96a3
rodrigo1619 <00067920@uca.edu.sv>
2023-01-20 15:39:20 -0600

    propTypes.default

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({title, subtitle}) => {
+export const FirstApp = ({title, subtitle, name}) => {
     //console.log(props);
     return (
         <>
             <h1> {title} </h1>
             {/**< </h1>  */}
             <p>{subtitle}</p>
+            <p>{name}</p>
         </>
 
     )
@@ -14,5 +15,11 @@ export const FirstApp = ({title, subtitle}) => {
 //Para poder agegarle tipado y que sea obligatorio mandar la propiedad
 FirstApp.propTypes ={
     title: PropTypes.string.isRequired,
-    subtitle: PropTypes.number.isRequired,
+    subtitle: PropTypes.number,
+}
+
+FirstApp.defaultProps ={
+    title: 'Hola camaron sin cola',
+    subtitle: 123,
+    name: 'Rodrigo Molina'
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title='ola' subtitle={123} />
+        <FirstApp />
     </React.StrictMode>
 )
