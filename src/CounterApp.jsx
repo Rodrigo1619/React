c9fe3a0bbf0824fa4494ac845b34d9ac63aa33fe
rodrigo1619 <00067920@uca.edu.sv>
2023-01-29 15:37:23 -0600

    feat: tests finished

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -27,7 +27,7 @@ export const CounterApp = ({value})=>{
 
             <button onClick={ handleAdd }>+1</button>
             <button onClick={ handleLess }>-1</button>
-            <button onClick={ handleReset }>Reset</button>
+            <button aria-label='btn-reset' onClick={ handleReset }>Reset</button>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/*<CounterApp value={10} />*/}
-        <FirstApp title='Ola camaron'/>
+        <CounterApp value={10} />
+        
     </React.StrictMode>
 )
